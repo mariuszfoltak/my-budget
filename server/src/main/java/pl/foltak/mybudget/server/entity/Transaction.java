79f22d392a9ab79697047dc25664d41cdeff538d
Mariusz Foltak <mariusz@foltak.pl>
2014-11-22 10:07:26 +0100

    Implement methods for Transaction entity

--- server/src/main/java/pl/foltak/mybudget/server/entity/Transaction.java
+++ server/src/main/java/pl/foltak/mybudget/server/entity/Transaction.java
@@ -1,27 +1,39 @@
 package pl.foltak.mybudget.server.entity;
 
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+import lombok.NonNull;
 
 /**
+ * Transaction entity.
  *
  * @author mariusz@foltak.pl
  */
 public class Transaction {
 
+    List<Tag> tags;
+
     public long getId() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public List<String> getTags() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    /**
+     * Returns and unmodifiable list of tags that belongs to this objects.
+     *
+     * @return list of tags
+     */
+    public List<Tag> getTags() {
+        return Collections.unmodifiableList(tags);
     }
 
-    public void addTag(Tag secondTag) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    /**
+     * Adds tag to this object. If the tag is null throws {@link java.lang.NullPointerException}.
+     *
+     * @param tag
+     */
+    public void addTag(@NonNull Tag tag) {
+        tags.add(tag);
     }
 
-    public String getCategoryPath() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
 }
---
+++ server/src/test/java/pl/foltak/mybudget/server/entity/TransactionTest.java
@@ -0,0 +1,54 @@
+package pl.foltak.mybudget.server.entity;
+
+import java.util.LinkedList;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Test of Transaction entity.
+ *
+ * @author mariusz@foltak.pl
+ */
+public class TransactionTest {
+
+    private Transaction instance;
+
+    @Before
+    public void setUp() {
+        instance = new Transaction();
+    }
+
+    /**
+     * The getTags() should return a copy of tags list,
+     */
+    @Test
+    public void isReturnedCopyOfTagsListWhenGetTagsIsCalled() {
+        instance.tags = new LinkedList<>();
+        assertNotSame("List must be a copy", instance.tags, instance.getTags());
+    }
+
+    /**
+     * The addTag() should add a tag to tags list.
+     */
+    @Test
+    public void isTagAddedToListAddTagMethodIsCalled() {
+        final Tag tag = mock(Tag.class);
+        instance.tags = mock(List.class);
+        instance.addTag(tag);
+        verify(instance.tags).add(tag);
+    }
+
+    /**
+     * The addTag should throw a NullPointerException when parameter is null.
+     */
+    @Test(expected = NullPointerException.class)
+    public void isNullPointerExceptionThrownWhenAddTagWithNullParameterIsCalled() {
+        instance.tags = mock(List.class);
+        instance.addTag(null);
+    }
+
+}
