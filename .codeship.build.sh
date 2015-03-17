18aead90a902d4a1d35a7d938992e4076792b8c9
Mariusz Foltak <mariusz@foltak.pl>
2015-03-17 09:47:13 +0100

    Give Jboss some time to adds mysql driver

--- .codeship.build.sh
+++ .codeship.build.sh
@@ -2,7 +2,7 @@
 cd ..
 mvn dependency:unpack -Dartifact=org.wildfly:wildfly-dist:8.1.0.Final:zip -DoutputDirectory=.
 nohup bash -c "wildfly-8.1.0.Final/bin/standalone.sh 2>&1 &" && sleep 4
-mvn dependency:copy -Dartifact=mysql:mysql-connector-java:5.1.34 -DoutputDirectory=wildfly-8.1.0.Final/standalone/deployments/
+mvn dependency:copy -Dartifact=mysql:mysql-connector-java:5.1.34 -DoutputDirectory=wildfly-8.1.0.Final/standalone/deployments/ && sleep 4
 wildfly-8.1.0.Final/bin/jboss-cli.sh -c "/subsystem=datasources/data-source=mybudgetDS:add(\
 driver-name=mysql-connector-java-5.1.34.jar_com.mysql.jdbc.Driver_5_1,\
 user-name=$MYSQL_USER,\
